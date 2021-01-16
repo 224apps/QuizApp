cb027d19c350f54e5e7574f74039c84b6d6fcc88
224Apps <224apps@gmail.com>
2021-01-15 23:42:02 -0500

    Powerful Refactorings in Swift and Tested the notification of the delegate

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -7,19 +7,22 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController, UITableViewDataSource {
+class QuestionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
     
-    private var question: String = ""
+    private var question = ""
     private var options: [String] = []
+    private let reuseIdentifier = "Cell"
+    private var selection: ((String)->Void)? = nil
     
-    convenience init(question: String, options: [String]) {
+    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
         self.init()
         self.question = question
         self.options = options
+        self.selection = selection
     }
     
     override func viewDidLoad() {
@@ -33,9 +36,24 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = dequeCell(in: tableView)
         cell.textLabel?.text = options[indexPath.row ]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection?(options[indexPath.row])
+    }
+    
+    
+    
+    //MARK: - Helper methods
+    
+    private func dequeCell(in tableView: UITableView) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return cell
+        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
     }
 }
 
